cb8fe9ede83609994013584bc350a38f2e43a9c2
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-10-10 23:19:35 +0530

    updated js and css

--- app.js
+++ app.js
@@ -57,7 +57,17 @@ function drawWeather(d) {
         document.getElementById('showFahrenheit').innerHTML = '&deg;' + 'C' ; 
       });
 
-      }
+    }
+
+    let d = new Date();
+
+    console.log(d.getDay())
+
+    
+
+
+
+    // getDate();
       
 
 
