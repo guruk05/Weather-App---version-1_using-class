043eabf570f126828378ac2cda51e4c1cef31795
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-10-10 21:40:26 +0530

    updated js

--- app.js
+++ app.js
@@ -2,47 +2,59 @@ const key = '2ebbc925a11c3ea05fe72bb48f8ad49d';
 let cityName;
 
 function weatherBallon(city) {
-    let cityName = document.getElementById("cityName").value;
-    // console.log(cityName);
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + key)
+  let cityName = document.getElementById("cityName").value;
+  // console.log(cityName);
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + key)
     // console.log(cityName)  
-	.then(function(resp) { return resp.json() }) // Convert data to json
-	.then(function(data) {
-        // console.log(data);
-        drawWeather(data);
-        showFahrenheit(data)
+    .then(function (resp) {
+      return resp.json()
+    }) // Convert data to json
+    .then(function (data) {
+      // console.log(data);
+      drawWeather(data);
+      // showFahrenheit(data);
 
-	})
-	.catch(function() {
-        // catch any errors
-	});
+    })
+    .catch(function () {
+      // catch any errors
+    });
 }
 
 
-function drawWeather( d ) {
-  let celcius = Math.round(parseFloat(d.main.temp)-273.15);	
-  let description = d.weather[0].description; 
+function drawWeather(d) {
+  console.log(d);
+  let celcius = Math.round(parseFloat(d.main.temp) - 273.15);
+  let description = d.weather[0].description;
   console.log(d)
   let country = d.sys.country;
-//   console.log(d.name);
+  //   console.log(d.name);
 
-	
-    document.getElementById('showDescription').innerHTML = description;
-    document.getElementById('showDegree').innerHTML = celcius;
-    document.getElementById('showCity').innerHTML = d.name;
-    document.getElementById('showCountry').innerHTML = country;   
+
+  document.getElementById('showDescription').innerHTML = description;
+  document.getElementById('showDegree').innerHTML = celcius;
+  document.getElementById('showCity').innerHTML = d.name;
+  document.getElementById('showCountry').innerHTML = country;
+
+$('.Celsius').on('click',function (){
+  let fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32);
+  console.log(fahrenheit);   
+  document.getElementById('showDegree').innerHTML = fahrenheit; 
+});
 }
-    
+
+
 
 // window.onload = function() {
 // 	weatherBallon('chennai');
 // }
 
-// function showFahrenheit( d ){
+// function showFahrenheit(){
+//     weatherBallon(city)
+    
 //     let fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32);
-//     // console.log(fahrenheit);
-//     document.getElementById('showFahrenheit').innerHTML = fahrenheit; 
 //     console.log(fahrenheit);
+//     document.getElementById('showFahrenheit').innerHTML = fahrenheit; 
+//     // console.log(fahrenheit);
 // }
 
 // document.getElementsByClassName("showFahrenheit")[0].addEventListener("click", function(){
